2e63a74566e5bd44910fcb8e28f8c905b03c4220
learn-co-kat <twothistles@gmail.com>
2019-11-12 10:40:27 -0800

    Created controller actions for stories and writers.

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,2 +1,39 @@
 class StoriesController < ApplicationController
+
+    def index
+        @stories = Story.all
+    end 
+
+    def new
+        @story = Story.new 
+    end 
+
+    def create
+        @story = Story.create(story_params) 
+    end 
+
+    def show 
+        @story = Story.find(params[:id]) 
+    end 
+
+    def edit
+        @story = Story.find(params[:id]) 
+    end 
+
+    def update
+        @story = Story.find(params[:id]) 
+        @story = Story.update(story_params) 
+        redirect_to story_path 
+    end 
+
+    def delete
+
+    end 
+
+    private
+
+    def story_params
+        params.require(:story).permit(:content, :subject, :writer_id, :country_id) 
+    end 
+
 end
--- app/controllers/writers_controller.rb
+++ app/controllers/writers_controller.rb
@@ -1,2 +1,25 @@
 class WritersController < ApplicationController
+
+    def index
+        @writers = Writer.all
+    end 
+
+    def new
+        @writer = Writer.new 
+    end 
+
+    def create
+        @writer = Writer.create(writer_params) 
+    end 
+
+    def show
+        @writer = Writer.find(params[:id]) 
+    end 
+
+    private
+
+    def writer_params
+        params.require(:writer).permit(:name, :nationality, :age)
+    end 
+
 end
