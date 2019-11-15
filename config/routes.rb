12a4653280db0617538d0fbabe0be8ff9860b39c
learn-co-kat <twothistles@gmail.com>
2019-11-14 16:30:20 -0800

    Working on tags show page. Currently displays correct tag.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :stories
   resources :writers
-  resources :tags
+  resources :tag
   get '/', :to => 'home#show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
